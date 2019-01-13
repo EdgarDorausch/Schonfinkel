4fd1369e1a9b756753fa9773ad1de79220b542c6
Edgar Dorausch <EdgarDorausch@web.de>
2019-01-13 16:18:49 +0100

    Add JSDoc to the functions

--- src/option/utils.ts
+++ src/option/utils.ts
@@ -3,6 +3,10 @@ import { Fun1, Fun2, Fun3, Fun4 } from '../types';
 
 type OptionObject<T> = {[k in keyof T]: Option<T[k]>}
 
+/**
+ * 'Pull' out all `Option` properties of an object and return an Option of an object with the option-unwrapped
+ *  properties of given object
+ */
 export function zip<T extends {}>(obj: OptionObject<T>): Option<T> {
   let newObj = {} as any;
   
@@ -20,16 +24,25 @@ export function zip<T extends {}>(obj: OptionObject<T>): Option<T> {
   return some(newObj);
 }
 
+/**
+ * Transforms a function in such a way that it can process options
+ */
 export function lift1<A,B>(f: Fun1<A,B>): Fun1<Option<A>,Option<B>> {
   return (optA) => optA.map(a => f(a))
 }
 
+/**
+ * Transforms a function in such a way that it can process options
+ */
 export function lift2<A,B,C>(f: Fun2<A,B,C>): Fun2<Option<A>,Option<B>,Option<C>> {
   return (optA, optB) => optA.flatMap(
     a => optB.map(
     b => f(a,b)))
 }
 
+/**
+ * Transforms a function in such a way that it can process options
+ */
 export function lift3<A,B,C,D>(f: Fun3<A,B,C,D>): Fun3<Option<A>,Option<B>,Option<C>,Option<D>> {
   return (optA, optB, optC) => optA.flatMap(
     a => optB.flatMap(
@@ -37,6 +50,9 @@ export function lift3<A,B,C,D>(f: Fun3<A,B,C,D>): Fun3<Option<A>,Option<B>,Optio
     c => f(a,b,c))))
 }
 
+/**
+ * Transforms a function in such a way that it can process `Option's`
+ */
 export function lift4<A,B,C,D,E>(f: Fun4<A,B,C,D,E>): Fun4<Option<A>,Option<B>,Option<C>,Option<D>,Option<E>> {
   return (optA, optB, optC, optD) => optA.flatMap(
     a => optB.flatMap(
@@ -45,6 +61,9 @@ export function lift4<A,B,C,D,E>(f: Fun4<A,B,C,D,E>): Fun4<Option<A>,Option<B>,O
     d => f(a,b,c,d)))))
 }
 
+/**
+ * Applicative apply method. Pass some function inputs into a function which is wrapped by an `Option`
+ */
 export function apply<A,B>(optF: Option<Fun1<A,B>>): (optA: Option<A>|A) => Option<B> {
   return (optA) => {
     if (optA instanceof Some || optA instanceof None) {
